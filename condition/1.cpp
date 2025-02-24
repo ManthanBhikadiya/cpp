dc0540a8a1435925ca65fbbbdf4730fd502f0b06
unknown <manthanbhikadiya9931@gmail.com>
2025-02-24 13:34:56 +0530

    condition

---
+++ condition/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int b;
+
+    cout << "Enter value of a: ";
+    cin >> a;
+    cout << "Enter value of b: ";
+    cin >> b;
+
+    if (a > b)
+    {
+        cout << "a is greater than b" << endl;
+    }
+    else
+    {
+        cout << "b is greater than a" << endl;
+    }
+    return 0;
+}
