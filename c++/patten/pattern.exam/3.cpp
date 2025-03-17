f58cb5ca5cce3d0ad05a0010f593979cbb991e87
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2025-03-17 11:14:39 +0530

    c++

---
+++ c++/patten/pattern.exam/3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int n = 5;
+
+    for (int i = 1; i <= n * n; i++)
+    {
+        if (i <= n || i >= n * (n - 1))
+        {
+            sum += i;
+        }
+        else
+        {
+            if (i % n == 1 || i % n == 0)
+            {
+                sum += i;
+            }
+            else
+            {
+            }
+        }
+
+        if (i % n == 0)
+        {
+        }
+    }
+    cout << "sum of pattern:" << sum;
+}
