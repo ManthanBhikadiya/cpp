eba04f106c27c5fae5061eee0e1c46fa5dc51946
ManthanBhikadiya <manthanbhikadiya9931@gmail.com>
2025-04-03 11:51:08 +0530

    Q-4.cpp

--- c++/Assortment/Q-4.cpp
+++ c++/Assortment/Q-4.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include<vector>
 using namespace std;
 
 int main(){
@@ -11,7 +10,7 @@ int main(){
     cout << "Enter the array's column size:";
     cin >> c;
 
-    vector<vector<int>> a(r, vector<int>(c));
+    int a[r][c];
 
     cout << "Enter array's elements: ";
     for (int i = 0; i < r; i++)
